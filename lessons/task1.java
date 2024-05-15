c0ddaf68519b371c0c9b420fa62fed5bbd023ef8
RaeNQA <raenqa@gmail.com>
2024-05-15 18:49:20 +0300

    printThreeWords() method creation

---
+++ lessons/task1.java
@@ -0,0 +1,11 @@
+public class task1 {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");            // or System.out.println ("Orange,\nBanana,\nApple")
+    }
+}

6579a749b963bf8b6f3ffbab082de4f50b092c4e
RaeNQA <raenqa@gmail.com>
2024-05-17 23:19:52 +0300

    Renaming

---
+++ tasks/task7.java
@@ -0,0 +1,11 @@
+package tasks;
+
+public class task7 {
+    public static void main(String[] args) {
+        System.out.println(xnum(1));
+        System.out.println(xnum(-10));
+    }
+    public static boolean xnum (int a){
+        return a > 0;
+    }
+}

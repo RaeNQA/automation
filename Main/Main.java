0516afa41231a2f586d7773254fee48e7843707d
RaeNQA <raenqa@gmail.com>
2024-05-22 17:13:16 +0300

    Renaming

--- Main/Main.java
+++ Main/Main.java
@@ -2,19 +2,6 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        task1.printThreeWords();
-        task2.checkSumSign();
-        task3.printColor();
-        task4.compareNumbers();
-        task5.main();
-        task6.main();
-        task7.main();
-        task8.main();
-        task9.main();
-        task10.main();
-        task11.main();
-        task12.main();
-        task13.main();
-        task14.main();
+
     }
 }
--- Main/task1.java
+++ Main/task1.java
@@ -3,12 +3,5 @@ package Main;
 public class task1 {
 
     public static void main(String[] args) {
-        printThreeWords();
-    }
-
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
     }
 }

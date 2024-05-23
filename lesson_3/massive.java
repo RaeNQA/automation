9878f5b0ec634ada30840d1057a194cbacd83090
RaeNQA <raenqa@gmail.com>
2024-05-23 17:39:13 +0300

    Renaming

--- lesson_3/massive.java
+++ lesson_3/massive.java
@@ -1,4 +1,35 @@
 package lesson_3;
 
 public class massive {
+    private String fullName;
+    private String position;
+    private String eMail;
+    private String number;
+    private double salary;
+    private int age;
+
+    public massive(String fullName, String position, String eMail, String number, double salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.eMail = eMail;
+        this.number = number;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void displ() {
+        System.out.println("ФИО: " + this.fullName);
+        System.out.println("Должность: " + position);
+        System.out.println("Email: " + eMail);
+        System.out.println("Телефон: " + number);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
+    }
+
+    public static void main() {
+        massive empDisplay = new massive("Yanov Yanovich Yan", "AQA", "Test@mail.asd", "+12345566452", 50000, 33);
+        empDisplay.displ();
+    }
 }
+
+

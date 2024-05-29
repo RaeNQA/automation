49bfdacfcbf773c99c39aa993ea99f447c36cdec
RaeNQA <raenqa@gmail.com>
2024-05-29 17:36:35 +0300

    Renaming

--- lesson_4/task2/Calculate.java
+++ lesson_4/task2/Calculate.java
@@ -1,8 +1,20 @@
 package lesson_4.task2;
 
 interface Calculate {
-    double cPerim();         // Расчет периметра
+   // double cPerim();         // Расчет периметра
     double cArea();          // Расчет площади
     String getfColor();         // Запрос цвета заливки
     String getbColor();         // Запрос цвета границы
+
+    default void Display(){
+        System.out.println("Периметр: " + this.cPerim());
+        System.out.println("Площадь: " + this.cArea());
+        System.out.println("Цвет фона: " + this.getfColor());
+        System.out.println("Цвет границ: " + this.getbColor());
+    }
+   default double cPerim(){
+       return 0;
+   }
+
 }
+
--- lesson_4/task2/Main.java
+++ lesson_4/task2/Main.java
@@ -2,30 +2,20 @@ package lesson_4.task2;
 
 public class Main {
     public static void main(String[] args) {
-        Calculate krug = new Krug(10, "Синий", "Красный");
+        Calculate krug = new Krug(4, "Синий", "Красный");
         Calculate pryamoug = new Pryamoug(4, 6, "Белый", "Зелёный");
         Calculate treug = new Treug(3, 4, 5, "Желтый", "Розовый");
 
         System.out.println("Круг:");
-        System.out.println("Периметр: " + krug.cPerim());
-        System.out.println("Площадь: " + krug.cArea());
-        System.out.println("Цвет фона: " + krug.getfColor());
-        System.out.println("Цвет границ: " + krug.getbColor());
-
+        krug.Display();
         System.out.println();
 
         System.out.println("Прямоугольник:");
-        System.out.println("Периметр: " + pryamoug.cPerim());
-        System.out.println("Площадь: " + pryamoug.cArea());
-        System.out.println("Цвет фона: " + pryamoug.getfColor());
-        System.out.println("Цвет границ: " + pryamoug.getbColor());
-
+        pryamoug.Display();
         System.out.println();
 
         System.out.println("Треугольник:");
-        System.out.println("Периметр: " + treug.cPerim());
-        System.out.println("Площадь: " + treug.cArea());
-        System.out.println("Цвет фона: " + treug.getfColor());
-        System.out.println("Цвет границ: " + treug.getbColor());
+        treug.Display();
+        treug.cPerim();
     }
 }

a3b6582dd021a913cd19e4ccdd359ba15b5033ad
RaeNQA <raenqa@gmail.com>
2024-05-29 18:49:15 +0300

    Renaming

--- lesson_4/task2/Calculate.java
+++ lesson_4/task2/Calculate.java
@@ -6,15 +6,16 @@ interface Calculate {
     String getfColor();         // Запрос цвета заливки
     String getbColor();         // Запрос цвета границы
 
+    // реализация метода Display в дефолтный для вывода информации о фигуре
     default void Display(){
         System.out.println("Периметр: " + this.cPerim());
         System.out.println("Площадь: " + this.cArea());
         System.out.println("Цвет фона: " + this.getfColor());
         System.out.println("Цвет границ: " + this.getbColor());
     }
+    // реализация метода calculate Perimetr(cPerim) как дэфолтный.
    default double cPerim(){
        return 0;
    }
-
 }
 

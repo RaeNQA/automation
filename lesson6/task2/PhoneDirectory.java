278a56d3184d7240283c9c5b33e951bf6d08b0af
RaeNQA <raenqa@gmail.com>
2024-06-06 16:34:08 +0300

    Renaming

--- lesson6/task2/PhoneDirectory.java
+++ lesson6/task2/PhoneDirectory.java
@@ -1,6 +1,9 @@
 package lesson6.task2;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PhoneDirectory {
     private Map<String, List<String>> directory;
